7f3a96dafd2e9eccee3b88b54391e25b083da2f4
Tigran <tigmir5@gmail.com>
2024-05-27 10:25:40 +0400

    portrait locking

--- app/src/main/java/com/timetwist/utils/ActivityUtils.java
+++ app/src/main/java/com/timetwist/utils/ActivityUtils.java
@@ -65,6 +65,7 @@ public class ActivityUtils {
 
     public static void configureLoginRegisterToMainActivity(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
     }
 
